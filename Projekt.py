dc0a97578f3b6817b953191df654eb4df83deecb
GENGAOG1 <g9645427@outlook.de>
2026-07-17 11:21:33 +0200

    Change PASSWORD variable value

--- Projekt.py
+++ Projekt.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template_string, redirect, url_for
 
 app = Flask(__name__)
 
-PASSWORD = "Fuck Society"
+PASSWORD = "Fynn"
 
 LOGIN_PAGE = """
 <!DOCTYPE html>
