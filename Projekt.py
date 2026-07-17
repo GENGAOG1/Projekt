30dcb67f492ab5cf0fbff7aef65fb8dc2508fa4c
GENGAOG1 <g9645427@outlook.de>
2026-07-17 18:16:47 +0200

    Update Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -171,8 +171,8 @@ def login():
 
     if request.method == "POST":
         if request.form.get("password") == PASSWORD:
-            return "Holy idiot I just got your IP and logged it, Idiot"
-            return redirect("https://vm.tiktok.com/ZGd9711rQ/")
+            return "Holy idiot I just got your IP and logged it, Idiot")
+
             
         else:
             message = "Wrong password!"
