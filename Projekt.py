f797286e0ca1c8b1c63828b1febdd9a9ac6267d7
GENGAOG1 <g9645427@outlook.de>
2026-07-17 16:13:35 +0200

    Update Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template_string, redirect, url_for
+import os
+import time
+import requests
 
 app = Flask(__name__)
 
@@ -179,6 +182,6 @@ def success():
 if __name__ == "__main__":
     app.run(debug=True)
     
-    if __name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
